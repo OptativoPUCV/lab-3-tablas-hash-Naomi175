72ced90af179cdba1b4085eafd80043b0d188909
Your Name <you@example.com>
2025-04-19 13:17:41 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -9,22 +9,6 @@
 typedef struct HashMap HashMap;
 int enlarge_called=0;
 
-
-/* //en hashmap.h
-struct Pair {
-    char * key;
-    void * value;
-};
-*/
-
-
-/*
-La estructura HashMap cuenta con un arreglo de punteros 
-Pair* (buckets). Cada Pair a su vez cuenta con dos variables: 
-una clave (key) y el valor asociado (value). size corresponde 
-a la cantidad de elementos que tiene el mapa y capacity es el 
-tamaño actual del arreglo buckets.
-*/
 struct HashMap {
     Pair ** buckets;
     long size; //cantidad de datos/pairs en la tabla
@@ -39,14 +23,6 @@ Pair * createPair( char * key,  void * value) {
     return new;
 }
 
-/*
-Para poder ubicar datos dentro del arreglo, la tabla necesita 
-dos funciones: la función hash (para transformar claves en 
-posiciones) y la función is_equal (para comparar claves). En 
-este laboratorio consideraremos que las claves son de tipo 
-string (char*). Usaremos la siguiente función hash para 
-transformar claves en posiciones dentro del arreglo:
-*/
 long hash( char * key, long capacity) {
     unsigned long hash = 0;
      char * ptr;
@@ -56,10 +32,6 @@ long hash( char * key, long capacity) {
     return hash%capacity;
 }
 
-/*
-La función is_equal por su parte, simplemente debe indicar si 
-dos claves son iguales (retorna 1) o no lo son (retorna 0):
-*/
 int is_equal(void* key1, void* key2){
     if(key1==NULL || key2==NULL) return 0;
     if(strcmp((char*)key1,(char*)key2) == 0) return 1;
@@ -81,34 +53,19 @@ void insertMap(HashMap * map, char * key, void * value) {
     map->current = i;
 }
 
-/*
-Implemente la función void enlarge(HashMap * map). Esta función 
-agranda la capacidad del arreglo buckets y reubica todos sus
- elementos. Para hacerlo es recomendable mantener referenciado 
- el arreglo actual/antiguo de la tabla con un puntero auxiliar. 
- Luego, los valores de la tabla se reinicializan con un nuevo 
- arreglo con el doble de capacidad. Por último los elementos 
- del arreglo antiguo se insertan en el mapa vacío con el método 
- insertMap. 
- 
- Puede seguir los siguientes pasos:
-
-a - Cree una variable auxiliar de tipo Pair** para matener el 
-arreglo map->buckets (old_buckets);
-
-b - Duplique el valor de la variable capacity.
-
-c - Asigne a map->buckets un nuevo arreglo con la nueva capacidad.
-
-d - Inicialice size a 0.
-
-e - Inserte los elementos del arreglo old_buckets en el mapa 
-(use la función insertMap que ya implementó).
-*/
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
+    Pair ** old_buckets = map->buckets;
+    long antiguo_i = map->capacity;
 
+    map->capacity *= 2;
+    map->buckets = (Pair **)calloc(map->capacity, sizeof(Pair *));
+    map->size = 0;
 
+    for (long i = 0 ; i < antiguo_i ; i++)
+        if (old_buckets[i] != NULL && old_buckets[i]->key != NULL)
+            insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
+    free(old_buckets);
 }
 
 HashMap * createMap(long capacity) {
@@ -132,7 +89,6 @@ void eraseMap(HashMap * map,  char * key) {
             map->size--;
             return;
         }
-
         i = (i + 1) % map->capacity;
         if ( i == inicial_i) return;
     }
@@ -147,49 +103,26 @@ Pair * searchMap(HashMap * map,  char * key) {
             map->current = i;
             return map->buckets[i];
         }
-
         i = (i + 1) % map->capacity;
         if ( i == inicial_i) return NULL;
     }
     return NULL;
 }
 
-/*
-Implemente las funciones para recorrer la estructura: 
-Pair * firstMap(HashMap * map) retorna el primer Pair válido 
-del arreglo buckets. 
-
-Pair * nextMap(HashMap * map) retorna el siguiente Pair del 
-arreglo buckets a partir índice current. Recuerde actualizar 
-el índice.
-*/
 Pair * firstMap(HashMap * map) {
-    for (long i = 0 ; i < map->capacity ; i++) {
+    for (long i = 0 ; i < map->capacity ; i++) 
         if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
             map->current = i;
             return map->buckets[i];
         }
-    }
     return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
-    for (long i = map->current + 1 ; i < map->capacity ; i++) {
+    for (long i = map->current + 1 ; i < map->capacity ; i++)
         if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
             map->current = i;
             return map->buckets[i];
         }
-    }
     return NULL;
 }
-
-
-/*
-Ya implementó todas las funcionalides del TDA Mapa.
-Ahora ya puede comenzar a utilizar su mapa. Puede comenzar 
-viendo el código de ejemplo del archivo main.c. Para compilar 
-y ejecutar:
-gcc main.c hashmap.c -o main; ./main
-./main
-Y voilá!
-*/
