3735c2b00d2a1250f1b26bed3baabf3b7c96fd37
Your Name <you@example.com>
2025-04-19 11:10:21 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -9,6 +9,22 @@
 typedef struct HashMap HashMap;
 int enlarge_called=0;
 
+
+/* //en hashmap.h
+struct Pair {
+    char * key;
+    void * value;
+};
+*/
+
+
+/*
+La estructura HashMap cuenta con un arreglo de punteros 
+Pair* (buckets). Cada Pair a su vez cuenta con dos variables: 
+una clave (key) y el valor asociado (value). size corresponde 
+a la cantidad de elementos que tiene el mapa y capacity es el 
+tamaño actual del arreglo buckets.
+*/
 struct HashMap {
     Pair ** buckets;
     long size; //cantidad de datos/pairs en la tabla
@@ -23,6 +39,14 @@ Pair * createPair( char * key,  void * value) {
     return new;
 }
 
+/*
+Para poder ubicar datos dentro del arreglo, la tabla necesita 
+dos funciones: la función hash (para transformar claves en 
+posiciones) y la función is_equal (para comparar claves). En 
+este laboratorio consideraremos que las claves son de tipo 
+string (char*). Usaremos la siguiente función hash para 
+transformar claves en posiciones dentro del arreglo:
+*/
 long hash( char * key, long capacity) {
     unsigned long hash = 0;
      char * ptr;
@@ -32,41 +56,128 @@ long hash( char * key, long capacity) {
     return hash%capacity;
 }
 
+/*
+La función is_equal por su parte, simplemente debe indicar si 
+dos claves son iguales (retorna 1) o no lo son (retorna 0):
+*/
 int is_equal(void* key1, void* key2){
     if(key1==NULL || key2==NULL) return 0;
     if(strcmp((char*)key1,(char*)key2) == 0) return 1;
     return 0;
 }
 
+/*
+Implemente la función void insertMap(HashMap * map, char * key,
+ void * value). Esta función inserta un nuevo dato (key,value) 
+ en el mapa y actualiza el índice current a esa posición.
 
+Recuerde que para insertar un par (clave,valor) debe:
+
+a - Aplicar la función hash a la clave para obtener la posición 
+donde debería insertar el nuevo par.
+
+b - Si la casilla se encuentra ocupada, avance hasta una casilla 
+disponible (método de resolución de colisiones). Una casilla 
+disponible es una casilla nula, pero también una que tenga un par 
+inválido (key==NULL).
+
+c - Ingrese el par en la casilla que encontró.
+
+No inserte claves repetidas. Recuerde que el arreglo es circular. 
+Recuerde actualizar la variable size.
+*/
 void insertMap(HashMap * map, char * key, void * value) {
 
 
 }
 
+
+/*
+Implemente la función void enlarge(HashMap * map). Esta función 
+agranda la capacidad del arreglo buckets y reubica todos sus
+ elementos. Para hacerlo es recomendable mantener referenciado 
+ el arreglo actual/antiguo de la tabla con un puntero auxiliar. 
+ Luego, los valores de la tabla se reinicializan con un nuevo 
+ arreglo con el doble de capacidad. Por último los elementos 
+ del arreglo antiguo se insertan en el mapa vacío con el método 
+ insertMap. 
+ 
+ Puede seguir los siguientes pasos:
+
+a - Cree una variable auxiliar de tipo Pair** para matener el 
+arreglo map->buckets (old_buckets);
+
+b - Duplique el valor de la variable capacity.
+
+c - Asigne a map->buckets un nuevo arreglo con la nueva capacidad.
+
+d - Inicialice size a 0.
+
+e - Inserte los elementos del arreglo old_buckets en el mapa 
+(use la función insertMap que ya implementó).
+*/
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
 
 
 }
 
-
+/*
+Implemente la función createMap en el archivo hashmap.c. 
+Esta función crea una variable de tipo HashMap, 
+inicializa el arreglo de buckets con casillas nulas, 
+inicializa el resto de variables y retorna el mapa. 
+Inicialice el índice current a -1.
+*/
 HashMap * createMap(long capacity) {
 
     return NULL;
 }
 
+/*
+Implemente la función void eraseMap(HashMap * map, char * key). 
+Está función elimina el dato correspondiente a la clave key. 
+Para hacerlo debe buscar el dato y luego marcarlo para que no 
+sea válido. No elimine el par, sólo invalídelo asignando NULL 
+a la clave (pair->key=NULL). Recuerde actualizar la variable size.
+*/
 void eraseMap(HashMap * map,  char * key) {    
 
 
 }
 
+/*
+Implemente la función Pair * searchMap(HashMap * map, char * key), 
+la cual retorna el Pair asociado a la clave ingresada. Recuerde que 
+para buscar el par debe:
+
+a - Usar la función hash para obtener la posición donde puede 
+encontrarse el par con la clave.
+
+b - Si la clave no se encuentra avance hasta encontrarla (método 
+de resolución de colisiones).
+
+c - Si llega a una casilla nula, retorne NULL inmediatamente (no 
+siga avanzando, la clave no está).
+
+Recuerde actualizar el índice current a la posición encontrada. 
+Recuerde que el arreglo es circular.
+*/
 Pair * searchMap(HashMap * map,  char * key) {   
 
 
     return NULL;
 }
 
+/*
+Implemente las funciones para recorrer la estructura: 
+Pair * firstMap(HashMap * map) retorna el primer Pair válido 
+del arreglo buckets. 
+
+Pair * nextMap(HashMap * map) retorna el siguiente Pair del 
+arreglo buckets a partir índice current. Recuerde actualizar 
+el índice.
+*/
 Pair * firstMap(HashMap * map) {
 
     return NULL;
@@ -76,3 +187,14 @@ Pair * nextMap(HashMap * map) {
 
     return NULL;
 }
+
+
+/*
+Ya implementó todas las funcionalides del TDA Mapa.
+Ahora ya puede comenzar a utilizar su mapa. Puede comenzar 
+viendo el código de ejemplo del archivo main.c. Para compilar 
+y ejecutar:
+gcc main.c hashmap.c -o main; ./main
+./main
+Y voilá!
+*/
